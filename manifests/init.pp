5337e240653d4cb5ac190063f9ed6df2f88df515
Simon Josi <simon.josi@atizo.com>
2010-11-12 22:17:54 +0100

    add new copyright notice; switch to two space indentation

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,8 +2,9 @@
 # readahead module
 #
 # Copyright 2008, Puzzle ITC GmbH
+# Copyright 2010, Atizo AG
 # Marcel Härry haerry+puppet(at)puzzle.ch
-# Simon Josi josi+puppet(at)puzzle.ch
+# Simon Josi simon.josi+puppet(at)atizo.com
 #
 # This program is free software; you can redistribute 
 # it and/or modify it under the terms of the GNU 
@@ -11,23 +12,16 @@
 # the Free Software Foundation.
 #
 
-# modules_dir { "readahead": }
-
 class readahead {
-    include readahead::base
-}
-
-class readahead::base {
-    package{readahead:
-        ensure => present,
-    }
-
-    service{readahead_early:
-        enable => true, 
-        require => Package[readahead],
-    }
-    service{readahead_later:
-        enable => true, 
-        require => Package[readahead],
-    }
+  package{'readahead':
+    ensure => present,
+  }
+  service{'readahead_early':
+    enable => true, 
+    require => Package['readahead'],
+  }
+  service{'readahead_later':
+    enable => true, 
+    require => Package['readahead'],
+  }
 }
